51bccffbfa5cdfd655ba0d17dc16868cf96be6e3
Prohor722 <prohor.b722@gmail.com>
2022-05-22 15:17:08 +0600

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home it is</h2>
+            <button className='btn btn-primary '>Hohoho</button>
+        </div>
+    );
+};
+
+export default Home;
