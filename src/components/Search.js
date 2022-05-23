a2a18b05edcb13a74172db37460c1702cb267daa
Prohor722 <prohor.b722@gmail.com>
2022-05-23 15:32:36 +0600

    products page added and search implemented

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const Search = ({onSearch}) => {
-    return (
-        <div>
-            <form onSubmit={onSearch}>
-            <input type="text" name="search" placeholder="Type here" class="input input-bordered input-primary w-full max-w-xs" />
-            <input type="submit" className='btn btn-secondary'>Search</input>
-            </form>
-        </div>
-    );
+const Search = ({ setSearch }) => {
+  const onSearch = (event) => {
+    event.preventDefault();
+    const search = event.target.search.value;
+    event.target.search.value = "";
+    setSearch(search.toLowerCase());
+    // console.log(search);
+  };
+
+  return (
+    <div className="my-10">
+      <form onSubmit={onSearch} className="flex flex-col lg:flex-row justify-center">
+        <input
+          type="text"
+          name="search"
+          placeholder="Type here"
+          class="input input-bordered input-primary w-full lg:max-w-lg"
+        />
+        <button type="submit" className="btn btn-secondary mt-2 lg:mt-0 lg:ml-2">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Search;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,38 +1,45 @@
-import { data } from 'autoprefixer';
-import React, { useState } from 'react';
-import SingleProduct from '../components/Home/SingleProduct';
-import Search from '../components/Search';
+import React, { useEffect, useState } from "react";
+import SingleProduct from "../components/Home/SingleProduct";
+import Search from "../components/Search";
 
 const Products = () => {
-    const [products , setProducts] = useState([]);
-    // const { data:reviews} = useQuery('reviews', ()=>fetch('reviews.json').then(res=>res.json()));
-    fetch('products.json')
-    .then(res=>res.json())
-    .then(data=>setProducts(data))
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  // const { data:reviews} = useQuery('reviews', ()=>fetch('reviews.json').then(res=>res.json()));
 
-    const onSearch = (event) =>{
-        const search = event.target.search.value;
-        console.log(search);
-        const searchProducts = products.filter(p=>p.name.includes(search));
-        // if(searchProducts){
-        //     setProducts(searchProducts);
-        // }
+  useEffect(()=>{
+      fetch("products.json")
+        .then((res) => res.json())
+        .then((data) => setProducts(data));
+  },[search])
+
+  if(search.length>0){
+    const searchProducts = products.filter((product) => product.name.toLowerCase().includes(search));
+    console.log("Search items",searchProducts)
+
+    if(searchProducts) {
+        setProducts(searchProducts);
+        setSearch('');
     }
-    return (
-        <div className='my-20'>
-            <h2 className='text-4xl mb-10 font-semibold text-secondary text-center'>All Customer Reviews</h2>
+  }
 
-            <div>
-                <Search onSearch={onSearch}/>
-            </div>
+  return (
+    <div className="my-20">
+      <h2 className="text-4xl mb-10 font-semibold text-secondary text-center">
+        All Customer Reviews
+      </h2>
 
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 mx-2 lg:mx-20'>
-            {
-                products.map(p=><SingleProduct key={p._id} product={p}/>)
-            }
-            </div>
-        </div>
-    )
+      <Search setSearch={setSearch} />
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mx-2 lg:mx-20">
+        {products? products.map((p) => (
+          <SingleProduct key={p?._id} product={p} />
+        )):
+        <p className="text-center text-secondary text-4xl my-20">No product to show</p>
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Products;
