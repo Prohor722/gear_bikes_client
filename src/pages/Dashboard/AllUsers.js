cbcdcfa72823d594cc61070234b9ec34a3485742
Prohor722 <prohor.b722@gmail.com>
2022-05-25 06:02:30 +0600

    all users and manage all orders created

--- src/pages/Dashboard/AllUsers.js
+++ src/pages/Dashboard/AllUsers.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../components/Loading';
 
 const AllUsers = () => {
+    const { data: users, isLoading } = useQuery("users", () =>
+    fetch(`http://localhost:5000/users`).then((res) => res.json())
+  );
+
+  const makeAdmin = (id) =>{
+    fetch(`http://localhost:5000/makeAdmin/${id}`,{
+        method: "PUT",
+        headers:{
+            authorization: localStorage.getItem('accessToken')
+        }
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+    })
+  }
+
+  if (isLoading || !users) {
+    return <Loading />;
+  }
     return (
         <div>
             <h2>Al Users</h2>
+            <div>
+        <div class="overflow-x-auto">
+          <table class="table table-compact w-full">
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Phone</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users &&
+                users.map((u, index) => (
+                  <tr key={u?._id}>
+                    <th>{index + 1}</th>
+                    <th>
+                      <div class="mask mask-squircle w-20 h-20">
+                        <img
+                          src={u?.img}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </th>
+                    <th>{u?.name}</th>
+                    <th>{u?.email}</th>
+                    <th>{u?.address}</th>
+                    <th>{u?.phone}</th>
+                    <th>
+                      { !u?.name ? 
+                      <p className="text-success">{u?.role}</p>  
+                       : 
+                       <button className="btn btn-sm btn-success" onClick={()=>makeAdmin(u?._id)}>Make Admin</button>
+                      }
+                    </th>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
         </div>
     );
 };
--- src/pages/Dashboard/ManageOrders.js
+++ src/pages/Dashboard/ManageOrders.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../../components/Loading";
 
 const ManageOrders = () => {
-    return (
-        <div>
-            <h2>Manage Orders</h2>
+  const { data: orders, isLoading } = useQuery("allOrders", () =>
+    fetch(`http://localhost:5000/orders`).then((res) => res.json())
+  );
+
+  if (isLoading || !orders) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      <h2 className="text-xl font-semibold text-secondary mb-2 text-center">
+        Manage Orders
+      </h2>
+      <div>
+        <div class="overflow-x-auto">
+          <table class="table table-compact w-full">
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Image</th>
+                <th>Product Name</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Phone</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders &&
+                orders.map((o, index) => (
+                  <tr key={o?._id}>
+                    <th>{index + 1}</th>
+                    <th>
+                      <div class="mask mask-squircle w-20 h-20">
+                        <img
+                          src={o?.productImg}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </th>
+                    <th>{o?.productName}</th>
+                    <th>{o?.email}</th>
+                    <th>{o?.address}</th>
+                    <th>{o?.phone}</th>
+                    <th>{o?.quantity}</th>
+                    <th>{o?.price}</th>
+                    <th>
+                      {o?.status === "unpaid" ? (
+                        <button className="btn btn-success">pay</button>
+                      ) : (
+                        <p className="text-success">{o?.status}</p>
+                      )}
+                    </th>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ManageOrders;
